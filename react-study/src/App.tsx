d26e0140a8590880b02e579a31de31142afb7b07
yiseungyun <seungxxyun@gmail.com>
2025-06-17 13:49:44 +0900

    feat: day01 라우트 추가

--- react-study/src/App.tsx
+++ react-study/src/App.tsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Day01 from "./code/day01"
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/day01" element={<>1일차 미션</>} />
+        <Route path="/day01" element={<Day01 />} />
       </Routes>
     </BrowserRouter>
   )
