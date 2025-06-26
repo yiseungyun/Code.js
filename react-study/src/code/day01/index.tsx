a5bae722774d5f2b5bd80c327d58a48eb3ac76c8
yiseungyun <seungxxyun@gmail.com>
2025-06-26 16:44:19 +0900

    feat: API 요청 중 문제 생기면 컨텍스트로 받은 throwError 호출

--- react-study/src/code/day01/index.tsx
+++ react-study/src/code/day01/index.tsx
@@ -1,9 +1,10 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import SearchBar from "./components/SearchBar";
 import SearchList from "./components/SearchList";
 import type { User } from "./types";
 import useDebounce from "./useDebounce";
 import AutoCompleteList from "./components/AutoCompleteList";
+import { ErrorContext } from "./error/ErrorContext";
 
 export default function Day01() {
   const [searchTerm, setSearchTerm] = useState("");
@@ -11,6 +12,7 @@ export default function Day01() {
   const [autoCompleteList, setAutoCompleteList] = useState<User[]>([]);
   const [searchList, setSearchList] = useState<User[]>([]);
   const debouncedSearchTerm = useDebounce(searchTerm, 300);
+  const throwError = useContext(ErrorContext);
 
   useEffect(() => {
     if (debouncedSearchTerm === "" || debouncedSearchTerm === commitedSearchTerm) {
@@ -19,8 +21,16 @@ export default function Day01() {
     }
 
     fetch(`/api/users?search=${encodeURIComponent(debouncedSearchTerm)}&sort=name&order=asc`)
-      .then(res => res.json())
-      .then(data => setAutoCompleteList(data));
+      .then(res => {
+        if (!res.ok) {
+          return throwError("SERVER_ERROR");
+        }
+        return res.json();
+      })
+      .then(data => setAutoCompleteList(data))
+      .catch(() => {
+        throwError("NETWORK_ERROR");
+      });
   }, [debouncedSearchTerm]);
 
   function handleAutoComplete(event: React.ChangeEvent<HTMLInputElement>) {
@@ -32,6 +42,8 @@ export default function Day01() {
       const finalSearchTerm = event.currentTarget.value;
       if (finalSearchTerm !== "") {
         fetchSearchTerm(finalSearchTerm);
+      } else {
+        throwError("TYPING_ERROR");
       }
     }
   };
@@ -41,8 +53,21 @@ export default function Day01() {
     setAutoCompleteList([]);
     setCommittedSearchTerm(searchTerm);
     fetch(`/api/users?search=${encodeURIComponent(searchTerm)}&sort=name&order=asc`)
-      .then(res => res.json())
-      .then(data => setSearchList(data));
+      .then(res => {
+        if (!res.ok) {
+          return throwError("SERVER_ERROR");
+        }
+        return res.json();
+      })
+      .then(data => {
+        setSearchList(data);
+        if (data.length === 0) {
+          throwError("NOT_FOUND");
+        }
+      })
+      .catch(() => {
+        throwError("NETWORK_ERROR");
+      });
   }
 
   return (
